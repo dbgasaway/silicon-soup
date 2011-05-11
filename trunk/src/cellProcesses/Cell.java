ca4f20099419509480bdc6234f885edd8194cd40
listerdaniel006 <listerdaniel006@210db667-020e-75cf-7bed-e081597ae23f>
2011-05-11 19:02:32 +0000

    update, more processing/mem managing

--- trunk/src/cellProcesses/Cell.java
+++ trunk/src/cellProcesses/Cell.java
@@ -1,5 +1,7 @@
 package cellProcesses;
 
+import java.util.Arrays;
+
 public class Cell {
         private Code c;
         private int head;
@@ -24,6 +26,28 @@ public class Cell {
         public int getSize() {
         	return this.size;
         }
+        
+        public void setAlloc(int a) {
+        	this.alloc += a;
+        }
+        
+        public int getHead() {
+        	return head;
+        }
+        
+        public void setHead(int ix) {
+        	head = ix;
+        }
+        
+        public boolean allocate(int size) {
+        	int ix = soup.allocate(this, size + alloc);
+        	if(ix == -1) {
+        		return false;
+        	} else {
+        		alloc += size;
+        		return true;
+        	}
+        }
 }
 
 class CPU {
@@ -37,7 +61,9 @@ class CPU {
         private int cycles;
         private SoupManager soup;
         
+        /**NOP and template value*/
         private static final byte NOP0 = 0;
+        /**NOP and template value*/
         private static final byte NOP1 = 1;
         private static final byte ZERO = 2;
         private static final byte ADDACB = 3;
@@ -47,7 +73,22 @@ class CPU {
         private static final byte SEARCHF = 7;
         private static final byte SEARCHB = 8;
         private static final byte SEARCH = 9;
-        private static final byte DIVIDE = 0;
+        private static final byte DIVIDE = 10;
+        private static final byte MOVEAB = 11;
+        private static final byte LOADAB = 12;
+        private static final byte ALLOC = 13;
+        private static final byte PUSHA = 14;
+        private static final byte PUSHB = 15;
+        private static final byte PUSHC = 16;
+        private static final byte POPA = 17;
+        private static final byte POPB = 18;
+        private static final byte POPC = 19;
+        
+        /**contains all valid instructions*/
+        private static final byte[] VALID_INSTRUCTIONS = {NOP0, NOP1, ZERO, ADDACB, 
+        	JUMPF, JUMP, JUMPB, SEARCHF, SEARCHB, SEARCH, DIVIDE, MOVEAB, LOADAB, 
+        	ALLOC, PUSHA, PUSHB, PUSHC, POPA, POPB, POPC
+        };
 
         public CPU(int ip, SoupManager soup) {
                 this.ip = ip;
@@ -71,6 +112,8 @@ class CPU {
         }
 
         private void execute(byte b) {
+        	 byte[] template;
+        	 int ix;
                 switch(b) {
                 case ZERO:
                         c = 0;
@@ -79,9 +122,130 @@ class CPU {
                         this.b = a + c;
                         break;
                 case JUMP:
-                        
+                	    template = this.getTemplate();
+                        ix = search(template, OUT);
+                        ip = ix + template.length;
+                        break;
+                case JUMPF:
+                	template = this.getTemplate();
+                    ix = search(template, FORWARD);
+                    ip = ix + template.length;
+                    break;
+                case JUMPB:
+                	template = this.getTemplate();
+                    ix = search(template, BACK);
+                    ip = ix + template.length;
+                    break;
+                case SEARCH:
+                	template = this.getTemplate();
+            	    c = template.length;
+                    ix = search(template, OUT);
+                    a = ix;
+                    break;
+                case SEARCHB:
+                	template = this.getTemplate();
+            	    c = template.length;
+                    ix = search(template, BACK);
+                    a = ix;
+                    break;
+                case SEARCHF:
+                	template = this.getTemplate();
+            	    c = template.length;
+                    ix = search(template, FORWARD);
+                    a = ix;
+                    break;
+                case DIVIDE:
+                	//TODO:implement division
                 default:
                         break;
                 }
         }
+
+        /**The valid template values. One searches for the inverse of the template after
+         *  the ip, e.g. NOP1, NOP0 -> NOP0, NOP1*/
+		private static final byte[] TEMPLATE_VALUES = {NOP0, NOP1};
+        
+        /**returns the template after the current instruction*/
+		private byte[] getTemplate() {
+			int i;
+			for(i = 1; i < soup.getSoupSize(); i++) {
+				boolean containsTemplate = false;
+				for(byte b : TEMPLATE_VALUES) {
+					if(soup.getValue(ip + i) == b) {
+						containsTemplate = true;
+						break;
+					}
+				}
+				if(!containsTemplate) {
+					break;
+				}
+			}
+			byte[] base = soup.getRange(ip + 1, ip + i);
+			for(int k = 0; k < base.length; k++) {
+				switch(base[k]) {
+				case NOP0:
+					base[k] = NOP1;
+					break;
+				case NOP1:
+					base[k] = NOP0;
+					break;
+				default:
+					throw new IllegalArgumentException("Invalid template type");
+				}
+			}
+			return base;
+		}
+		
+		private static final int OUT = 0;
+		private static final int FORWARD = 1;
+		private static final int BACK = -1;
+		
+		/**returns the index of the nearest template
+		 * @param template - the template to search for
+		 * @param i - the search method: -1 is back, 0 is out, 1 is forward
+		 * @return the index of the nearest template, or ip if none is found*/
+        private int search(byte[] template, int i) {
+        	if(i == OUT) {
+        		return searchOut(template);
+        	} else if(i == BACK) {
+        		return searchBack(template);
+        	} else if(i == FORWARD) {
+        		return searchForward(template);
+        	} else {
+        		throw new IllegalArgumentException("Bad search type");
+        	}
+		}
+
+		private int searchOut(byte[] template) {
+        	if(template.length == 0) return ip;
+        	int a = searchBack(template);
+        	int b = searchForward(template);
+        	if(soup.getDist(a, ip) < soup.getDist(b, ip)) {
+        		return a;
+        	} else {
+        		return b;
+        	}
+		}
+
+		/**Returns the index of the nearest template after the ip*/
+		private int searchBack(byte[] template) {
+        	if(template.length == 0) return ip;
+			for(int i = -1; i > -soup.getSoupSize(); i--) {
+				if(Arrays.equals(template, soup.getRange(ip + i - template.length, ip + i))) {
+					return ip + i - template.length;
+				}
+			}
+			return ip;
+		}
+		
+		/**Returns the index of the nearest template after the ip*/
+		private int searchForward(byte[] template) {
+        	if(template.length == 0) return ip;
+			for(int i = 1; i < soup.getSoupSize(); i++) {
+				if(Arrays.equals(template, soup.getRange(ip + i, ip + i + template.length))) {
+					return ip + i - template.length;
+				}
+			}
+			return ip;
+		}
 }
--- trunk/src/cellProcesses/SoupManager.java
+++ trunk/src/cellProcesses/SoupManager.java
@@ -17,26 +17,115 @@ public class SoupManager {
 		codes = new ArrayList<Code>();
 	}
 	
-	public byte getValue(int ip) {
-		ip = ip % soup.length;
-		return soup[ip];
+	/**returns the value at ix with circular memory*/
+	public byte getValue(int ix) {
+		ix = ix % soup.length;
+		return soup[ix];
+	}
+	
+	/**returns the locked status at ix assuming circular memory*/
+	public boolean getLockVal(int ix) {
+		ix = ix % lockedMem.length;
+		return lockedMem[ix];
+	}
+	
+	/**returns the distance between two addresses in circular memory*/
+	public int getDist(int ix, int iy) {
+		ix = ix % SOUP_SIZE;
+		iy = iy % SOUP_SIZE;
+		return Math.abs(ix - iy);
 	}
 	
 	public byte[] getSoup() {
-		return soup;
+		return soup.clone();
 	}
 	
+	public int getSoupSize() {
+		return SOUP_SIZE;
+	}
+	
+	/**Rearranges soup memory to increase contiguous space*/
 	private void garbageCollect() {
 		//TODO: arranges memory to increase contiguous space
 	}
 	
-	private boolean addCell(Cell c) {
+	/**Adds a cell to the soup, and returns if it was successful*/
+	public boolean addCell(Cell c) {
 		int ix = findSpace(c.getSize());
 		//TODO: this
+		return true;
+	}
+	
+	private void protectMem(int start, int size) {
+		//TODO:this
 	}
 
+	
+	/**finds the largest contiuous space of size size
+	 * @param size - the size of the space to find
+	 * @return the index of the location containing the requested space, or -1
+	 *  if no such space exists*/
 	private int findSpace(int size) {
-		// TODO Auto-generated method stub
-		return 0;
+		for(int i = 0; i < lockedMem.length; i++) {
+			if(!getLockVal(i)) {
+				int space = getSpaceAt(i);
+				if(space >= size) {
+					return i;
+				}
+			}
+		}
+		return -1;
+	}
+	
+	/**Returns the contiguous free space at ix*/
+	private int getSpaceAt(int ix) {
+		int count = 0;
+		for(int i = 0; i < lockedMem.length; i++) {
+			if(!getLockVal(i + ix)) {
+				count++;
+			} else {
+				break;
+			}
+		}
+		return count;
+	}
+
+	/**Returns the range of values in the soup from ix to iy, circularly. iy must be
+	 *  greater than ix and the difference must be less that the soup size*/
+	public byte[] getRange(int ix, int iy) {
+		byte[] ret = new byte[iy - ix];
+		int i;
+		for(i = 0; ix <= iy; ix++, i++) {
+			ret[i] = this.getValue(ix);
+		}
+		return ret;
+	}
+
+	public int allocate(Cell c, int size) {
+		int head = c.getHead();
+		int space = getSpaceAt(head);
+		if(space >= size) {
+			c.setAlloc(space);
+		} else {
+			garbageCollect();
+			space = getSpaceAt(head);
+			if(space >= size) {
+				c.setAlloc(space);
+			} else {
+				while(space < size) {
+					killTop();
+					garbageCollect();
+					space = getSpaceAt(head);
+				}
+				c.setAlloc(space);
+			}
+		}
+		protectMem(head, head + space);
+		return head + space;
+	}
+	
+	public void killTop() {
+		Cell c = cells.removeLast();
+		
 	}
 }
