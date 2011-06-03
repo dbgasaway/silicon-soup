d31d9d8ba2bbc0223e66a128e9cca70ff229e529
listerdaniel006 <listerdaniel006@210db667-020e-75cf-7bed-e081597ae23f>
2011-06-03 17:55:00 +0000

    can bring out a selected gene to console

--- trunk/src/main/SoupGUI.java
+++ trunk/src/main/SoupGUI.java
@@ -1,12 +1,16 @@
 package main;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.*;
 
 import cellProcesses.Cell;
 import cellProcesses.SoupManager;
+import cellProcesses.Code;
 
 import java.util.Arrays;
 import java.util.Stack;
@@ -20,6 +24,7 @@ public class SoupGUI extends JFrame implements Runnable {
 	private SoupManager s;
 	private JPanel p;
 	private final JTextArea t;
+	private final JTextArea input;
 	private final JTextArea totalCycles;
 	private final JButton b;
 	private Stack<Cell> cells;
@@ -54,15 +59,46 @@ public class SoupGUI extends JFrame implements Runnable {
 				System.out.println("Running= " + isRunning);
 				if(!isRunning) {
 					for(byte[] b : s.get10Codes()) {
-						System.out.println(Arrays.toString(b));
+						System.out.println(Arrays.toString(Code.getCodeNameList(b)));
 					}
 				}
 			}
 		});
 		
+		input = new JTextArea();
+		input.setColumns(30);
+		input.setRows(1);
+		//input.
+		input.setLineWrap(false);
+		input.setText("hi");
+		input.setBackground(Color.CYAN);
+		input.addKeyListener(new KeyListener() {
+			@Override
+			public void keyPressed(KeyEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			@Override
+			public void keyReleased(KeyEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			@Override
+			public void keyTyped(KeyEvent arg0) {
+				char c = arg0.getKeyChar();
+				if(c == '\n' || c == 'r') {
+					Code cd = s.getCode(input.getText());
+					System.out.println(cd);
+					if(cd != null) System.out.println(Arrays.toString(Code.getCodeNameList(cd.getCode())));
+					input.setText("");
+				}
+			}			
+		});
+		
 		p.add(t);
 		p.add(totalCycles);
 		p.add(b);
+		p.add(input);
 
 		s = new SoupManager();
 		cells = new Stack<Cell>();
